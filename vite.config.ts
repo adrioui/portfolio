6f2bd93100ffc2cbad74ee147fa65cbea1dea77a
adrioui <adrifdlh@gmail.com>
2025-03-05 09:02:51 +0700

    fix(Config): :bug: Change base url from "portfolio" to "#page=1"

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean
   ),
-  base: "/portfolio/",
+  base: "/#page=1/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
